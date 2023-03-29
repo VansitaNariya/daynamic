209abc9146cee00972a0ef59831df65583b59bb6
vansita nariya <vansitanariya82@gmail.com>
2023-03-29 07:28:08 +0530

    Initial Commit

---
+++ daynamic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  daynamic
+//
+//  Created by R92 on 29/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
